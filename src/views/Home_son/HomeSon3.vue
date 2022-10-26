5c8c5c3031c0a8e924c0dc6d89adbbbda167f6b2
pyyi15@163.com <pyyi15@163.com>
2022-10-26 09:56:04 +0800

    555

--- src/views/Home_son/HomeSon3.vue
+++ src/views/Home_son/HomeSon3.vue
@@ -12,7 +12,27 @@
         </div>
       </div>
       <div>
-        
+        <v-card
+          class="home_collect_card"
+          v-for="item in collect_content"
+          :key="item.id"
+        >
+          <v-list-item-title class="headline_2_2">{{item.article_name}}</v-list-item-title>
+          <v-list-item-subtitle class="subtitle_recommand_1_2">{{item.author}}</v-list-item-subtitle>
+          <div class="recommand_book_2">{{item.book}}</div>
+          <div class="quote_recommand_2">
+            <div>
+              <p class="font-weight-black">Number of citation：{{item.quote_num}}</p>
+            </div>
+            <div style="margin-left: 20px;">
+              <p class="font-weight-black">Number of visits：{{item.page_view}}</p>
+            </div>
+          </div>
+          <div class="recommand_icon_fa_2">
+            <img src="../../assets/quote.png" class="recommand_icon_1_2" />
+            <img src="../../assets/art_sc_cancel2.png" class="recommand_icon_2_2" />
+          </div>
+        </v-card>
       </div>
     </div>
   </template>
@@ -20,21 +40,20 @@
   <script>
   export default {
     data() {
-      return {
-        focus_people: [
+      return {collect_content: [
           {
-            author_name: "NAME PANYUYI",
-            focus_icon: 'https://s1.ax1x.com/2022/10/24/x2o8Rx.jpg',
-            h_index: 70,
-            article_num: 80,
-            quote_num: 10809
+            article_name: "Curvature-Adaptive Meta-Learning for Fast Adaptation to Manifold Data",
+            author: "Zhi Gao,Yuwei Wu,Mehrtash T Harandi,Yunde Jia",
+            book: "IEEE Transactions on Pattern Analysis and Machine Intelligence (TPAMI) （2022）",
+            quote_num: 0,
+            page_view: 0
           },
           {
-            author_name: "YPWDPYM",
-            focus_icon: 'https://s1.ax1x.com/2022/10/24/xRmgw8.jpg',
-            h_index: 100,
-            article_num: 180,
-            quote_num: 18317
+            article_name: "Variational Deep Image Restoration",
+            author: "Jae Woong Soh, Nam Ik Cho",
+            book: "Computer Science、CCF A",
+            quote_num: 0,
+            page_view: 0
           }
         ]
       }
@@ -65,20 +84,62 @@
   }
   </script>
   
-  <style scoped lang="scss">
-    .focus_1 {
-      margin-left: vw(110);
-      color: #232f3d;
-      margin-top: vh(20);
-    }
-    .focus_2 {
-      margin-left: vw(20);
-      color: #232f3d;
-      margin-top: vh(20);
-    }
-    .focus_3 {
-      margin-left: vw(20);
-      color: #232f3d;
-      margin-top: vh(20);
-    }
-  </style>
+<style scoped lang="scss">
+  .focus_1 {
+    margin-left: vw(110);
+    color: #232f3d;
+    margin-top: vh(20);
+  }
+  .focus_2 {
+    margin-left: vw(20);
+    color: #232f3d;
+    margin-top: vh(20);
+  }
+  .focus_3 {
+    margin-left: vw(20);
+    color: #232f3d;
+    margin-top: vh(20);
+  }
+
+  .home_collect_card {
+    margin-left: vw(110);
+    margin-top: vh(40);
+    width: vw(1190);
+    padding-top: 10px;
+  }
+  .headline_2_2 {
+    margin-left: vw(20);
+    font-size: vw(25);
+    font-family: "Source Han Sans CN Normal", sans-serif;
+  }
+  .subtitle_recommand_1_2 {
+    margin-left: vw(20);
+    font-family: "Source Han Sans CN Normal", sans-serif;
+  }
+  .recommand_book_2 {
+    color: #455A64;
+    margin-left: vw(20);
+    margin-top: vh(10);
+    font-family: "Source Han Sans CN Normal", sans-serif;
+  }
+  .quote_recommand_2 {
+    margin-left: vw(20);
+    margin-top: vh(15);
+    display: flex;
+    font-family: "Source Han Sans CN Normal", sans-serif;
+  }
+  .recommand_icon_fa_2 {
+    display: flex;
+  }
+  .recommand_icon_1_2 {
+    margin-left: vw(1070);
+    width: vw(30);
+    margin-bottom: vh(30);
+  }
+  .recommand_icon_2_2 {
+    width: vw(31);
+    height: vw(31);
+    margin-left: vw(25);
+    margin-bottom: vh(30);
+  }
+</style>
