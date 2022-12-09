603a86b3f2204e11c51b8b5193a04b8349ba7ef8
yjlintp <995290439@qq.com>
2022-12-09 20:08:25 +0800

    all

--- src/views/Statistics/Rank.vue
+++ src/views/Statistics/Rank.vue
@@ -18,13 +18,14 @@
             <v-card class="d-block mx-auto rounded-lg" flat>
               <div class="tree-filter">
                 <v-treeview
-                  :open="[0]"
                   selectable
                   dense
                   selected-color="indigo"
                   v-model="fieldSelection"
                   :items="fieldOptions"
-                  @update="getScholarRank"
+                  selection-type="leaf"
+                  return-object
+                  open-all
                 ></v-treeview>
               </div>
             </v-card>
@@ -51,10 +52,10 @@
                   hide-default-footer
                   class="elevation-1"
                 >
-                  <template v-slot:item.name="{ item }">
-                    <div>{{ item.name }}</div>
-                    <div>{{ item.org }}</div>
-                    <div>{{ item.area }}</div>
+                  <template v-slot:item.name1="{ item }">
+                    <div><b>{{ item.name1 }}</b></div>
+                    <div>org:  {{ handleSubString(item.org) }}</div>
+                    <div>area: {{ handleSubString(item.interests) }}</div>
                   </template>
                 </v-data-table>
                 <v-pagination
@@ -76,55 +77,55 @@
 
       <v-tab-item key="2">
         <div class="rank-content">
-        <div class="field-filter">
-          <v-card class="d-block mx-auto rounded-lg" flat>
-            <div class="tree-filter">
-              <v-treeview
-                :open="[1]"
-                selectable
-                dense
-                selected-color="indigo"
-                :items="fieldOptions"
-              ></v-treeview>
-            </div>
-          </v-card>
-        </div>
-        <div class="rank-main-content">
-        <div class="table-filters">
-          <v-text-field
-            v-model="orgSearch"
-            append-icon="mdi-magnify"
-            label="Search"
-            single-line
-            hide-details
-          ></v-text-field>
-        </div>
-        <div class="table">
-          <div class="main-table">
-            <v-data-table
-              :search="orgSearch"
-              :headers="orgHeader"
-              :items="orgDesserts"
-              @page-count="orgpageCount = $event"
-              :page.sync="orgpage"
-              :items-per-page="15"
-              hide-default-footer
-              class="elevation-1"
-            >
-              <template v-slot:item.name="{ item }">
-                <div>{{ item.name }}</div>
-                <div>{{ item.org }}</div>
-                <div>{{ item.area }}</div>
-              </template>
-            </v-data-table>
-            <v-pagination
-              class="pagination"
-              v-model="orgpage"
-              :length="orgpageCount"
-            ></v-pagination>
+          <div class="field-filter">
+            <v-card class="d-block mx-auto rounded-lg" flat>
+              <div class="tree-filter">
+                <v-treeview
+                  :open="[1]"
+                  selectable
+                  dense
+                  selected-color="indigo"
+                  :items="fieldOptions"
+                ></v-treeview>
+              </div>
+            </v-card>
           </div>
-        </div>
-        </div>
+          <div class="rank-main-content">
+            <div class="table-filters">
+              <v-text-field
+                v-model="orgSearch"
+                append-icon="mdi-magnify"
+                label="Search"
+                single-line
+                hide-details
+              ></v-text-field>
+            </div>
+            <div class="table">
+              <div class="main-table">
+                <v-data-table
+                  :search="orgSearch"
+                  :headers="orgHeader"
+                  :items="orgDesserts"
+                  @page-count="orgpageCount = $event"
+                  :page.sync="orgpage"
+                  :items-per-page="15"
+                  hide-default-footer
+                  class="elevation-1"
+                >
+                  <template v-slot:item.name="{ item }">
+                    <div>{{ item.name }}</div>
+                    <div>{{ item.org }}</div>
+                    <div>{{ item.interests }}</div>
+                  </template>
+                </v-data-table>
+                <v-pagination
+                  class="pagination"
+                  v-model="orgpage"
+                  :length="orgpageCount"
+                ></v-pagination>
+              </div>
+            </div>
+          </div>
         </div>
       </v-tab-item>
       <v-tab-item key="3"> 3 </v-tab-item>
@@ -133,7 +134,8 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
+import qs from "qs";
 export default {
   data() {
     const fieldOptions = [];
@@ -142,249 +144,97 @@ export default {
         text: "姓名",
         align: "start",
         sortable: false,
-        value: "name",
+        value: "name1",
+        width: 600,
       },
-      { text: "h指数", value: "hIndex", filterable: false },
-      { text: "论文数", value: "paperNum", filterable: false },
-      { text: "引用数", value: "citationNum", filterable: false },
+      { text: "h指数", value: "h_index", filterable: false, width: 120 },
+      { text: "论文数", value: "paperNum", filterable: false, width: 120 },
+      { text: "引用数", value: "citation", filterable: false, width: 120 },
     ];
-    const scholarDesserts = [
-      {
-        name: "Frozen Yogurt",
-        hIndex: 159,
-        paperNum: 6.0,
-        citationNum: 24,
-        org: "北京航空航天大学",
-        area: "人工智能",
-      },
-      {
-        name: "Ice cream sandwich",
-        hIndex: 237,
-        paperNum: 9.0,
-        citationNum: 37,
-        org: "北京航空航天大学",
-        area: "计算机 软件工程",
-      },
-      {
-        name: "Eclair",
-        hIndex: 262,
-        paperNum: 16.0,
-        citationNum: 23,
-        org: "北京航空航天大学",
-        area: "生物医学 化学合成 基因技术",
-      },
-      {
-        name: "Cupcake",
-        hIndex: 305,
-        paperNum: 3.7,
-        citationNum: 67,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "Gingerbread",
-        hIndex: 356,
-        paperNum: 16.0,
-        citationNum: 49,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "Jelly bean",
-        hIndex: 375,
-        paperNum: 0.0,
-        citationNum: 94,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "Lollipop",
-        hIndex: 392,
-        paperNum: 0.2,
-        citationNum: 98,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "Honeycomb",
-        hIndex: 408,
-        paperNum: 3.2,
-        citationNum: 87,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "Donut",
-        hIndex: 452,
-        paperNum: 25.0,
-        citationNum: 51,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "KitKat",
-        hIndex: 518,
-        paperNum: 26.0,
-        citationNum: 65,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "Ice cream sandwich",
-        hIndex: 237,
-        paperNum: 9.0,
-        citationNum: 37,
-        org: "北京航空航天大学",
-        area: "计算机 软件工程",
-      },
-      {
-        name: "Eclair",
-        hIndex: 262,
-        paperNum: 16.0,
-        citationNum: 23,
-        org: "北京航空航天大学",
-        area: "生物医学 化学合成 基因技术",
-      },
-      {
-        name: "Cupcake",
-        hIndex: 305,
-        paperNum: 3.7,
-        citationNum: 67,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "Gingerbread",
-        hIndex: 356,
-        paperNum: 16.0,
-        citationNum: 49,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "Jelly bean",
-        hIndex: 375,
-        paperNum: 0.0,
-        citationNum: 94,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "Lollipop",
-        hIndex: 392,
-        paperNum: 0.2,
-        citationNum: 98,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "Honeycomb",
-        hIndex: 408,
-        paperNum: 3.2,
-        citationNum: 87,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "Donut",
-        hIndex: 452,
-        paperNum: 25.0,
-        citationNum: 51,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-      {
-        name: "KitKat",
-        hIndex: 518,
-        paperNum: 26.0,
-        citationNum: 65,
-        org: "北京航空航天大学",
-        area: "AI",
-      },
-    ];
+
     const orgHeader = [
-        {
+      {
         text: "姓名",
         align: "start",
         sortable: false,
         value: "name",
-        },
-        { text: "国家", value: "country", filterable: false, sortable: false, },
-        { text: "2022自然指数", value: "index2022", filterable: false },
-        { text: "2022论文数", value: "count2022", filterable: false },
+      },
+      { text: "国家", value: "country", filterable: false, sortable: false },
+      { text: "2022自然指数", value: "index2022", filterable: false },
+      { text: "2022论文数", value: "count2022", filterable: false },
     ];
     const orgDesserts = [
-        {
-            name: 'Max Planck Society',
-            country: 'Germany',
-            index2022: 782.72,
-            count2022: 2780
-        },
-        {
-            name: 'French National Centre for Scientific Research (CNRS)',
-            country: 'France',
-            index2022: 675.69,
-            count2022: 4399
-        },
-        {
-            name: 'Helmholtz Association of German Research Centres',
-            country: 'Germany',
-            index2022: 565.07,
-            count2022: 2584
-        },
-        {
-            name: 'University of Oxford',
-            country: 'United Kingdom (UK)',
-            index2022: 452.28,
-            count2022: 1485
-        },
-        {
-            name: 'Swiss Federal Institute of Technology Zurich (ETH Zurich)',
-            country: 'Switzerland',
-            index2022: 395.67,
-            count2022: 1065
-        },
-        {
-            name: 'Imperial College London (ICL),',
-            country: 'United Kingdom (UK)',
-            index2022: 248.3,
-            count2022: 963
-        },
-        {
-            name: 'Russian Academy of Sciences (RAS)',
-            country: 'Russia',
-            index2022: 242.05,
-            count2022: 914
-        },
-        {
-            name: 'Swiss Federal Institute of Technology Lausanne (EPFL)',
-            country: 'Switzerland',
-            index2022: 228.37,
-            count2022: 619
-        },
-        {
-            name: 'UCL',
-            country: 'United Kingdom (UK)',
-            index2022: 222.6,
-            count2022: 981
-        },
-        {
-            name: 'Spanish National Research Council (CSIC)',
-            country: 'Spain',
-            index2022: 209.15,
-            count2022: 1190
-        },
-        {
-            name: 'Leibniz Association',
-            country: 'Germany',
-            index2022: 200.12,
-            count2022: 1001
-        }
+      {
+        name: "Max Planck Society",
+        country: "Germany",
+        index2022: 782.72,
+        count2022: 2780,
+      },
+      {
+        name: "French National Centre for Scientific Research (CNRS)",
+        country: "France",
+        index2022: 675.69,
+        count2022: 4399,
+      },
+      {
+        name: "Helmholtz Association of German Research Centres",
+        country: "Germany",
+        index2022: 565.07,
+        count2022: 2584,
+      },
+      {
+        name: "University of Oxford",
+        country: "United Kingdom (UK)",
+        index2022: 452.28,
+        count2022: 1485,
+      },
+      {
+        name: "Swiss Federal Institute of Technology Zurich (ETH Zurich)",
+        country: "Switzerland",
+        index2022: 395.67,
+        count2022: 1065,
+      },
+      {
+        name: "Imperial College London (ICL),",
+        country: "United Kingdom (UK)",
+        index2022: 248.3,
+        count2022: 963,
+      },
+      {
+        name: "Russian Academy of Sciences (RAS)",
+        country: "Russia",
+        index2022: 242.05,
+        count2022: 914,
+      },
+      {
+        name: "Swiss Federal Institute of Technology Lausanne (EPFL)",
+        country: "Switzerland",
+        index2022: 228.37,
+        count2022: 619,
+      },
+      {
+        name: "UCL",
+        country: "United Kingdom (UK)",
+        index2022: 222.6,
+        count2022: 981,
+      },
+      {
+        name: "Spanish National Research Council (CSIC)",
+        country: "Spain",
+        index2022: 209.15,
+        count2022: 1190,
+      },
+      {
+        name: "Leibniz Association",
+        country: "Germany",
+        index2022: 200.12,
+        count2022: 1001,
+      },
     ];
     return {
       get_fields: require("@/assets/json/get_fields.json"),
       scholarSearch: "",
-      fieldSelection: ref(),
+      fieldSelection: [],
       orgSearch: "",
       scholarpageCount: 0,
       orgpageCount: 0,
@@ -394,14 +244,21 @@ export default {
       scholarpage: 1,
       tab: null,
       fieldOptions,
-      scholarDesserts,
+      scholarDesserts: ref(),
       scholarHeader,
+      pushRank: ref(0),
     };
   },
-  methods: {
+  watch: {
+    fieldSelection(newVal) {
+      console.log(1);
+      this.getScholarRank();
+    },
+  },
 
+  methods: {
     getScholarFields() {
-          /*
+      /*
       this.$axios({
       method: "get",
       url: "/get_fields",
@@ -441,71 +298,84 @@ export default {
       });
       */
       const list = this.get_fields.sort();
-        this.fieldOptions = [];
-        this.fieldOptions.push({
-          name: '全部',
-          id: 0,
-          children: []
-        })
-        for (var index = 0; index < list.length; index++) {
-          if (list[index] instanceof Array) {
-            this.fieldOptions[0].children.push({
-              name: list[index][0],
-              id: index+1,
-              children: this.getAllFields(list[index],index)
-            })
-          }else {
-            this.fieldOptions[0].children.push({
-              name: list[index],
-              id: index+1
-            })
-          }
+      this.fieldOptions = [];
+      this.fieldOptions.push({
+        name: "全部",
+        id: 0,
+        children: [],
+      });
+      this.pushRank = 0;
+      for (var index = 0; index < list.length; index++) {
+        if (list[index] instanceof Array) {
+          this.fieldOptions[0].children.push({
+            name: list[index][0],
+            id: ++this.pushRank,
+            children: this.getAllFields(list[index], index),
+          });
+        } else {
+          this.fieldOptions[0].children.push({
+            name: list[index],
+            id: ++this.pushRank,
+          });
         }
+      }
     },
-    getAllFields(list,i) {
+    getAllFields(list, i) {
       var array = [];
       for (var index = 1; index < list.length; index++) {
-          if (list[index] instanceof Array) {
-            array.push({
-              name: list[index][0],
-              id: i+index+1,
-              children: this.getAllFields(list[index],i+index)
-            })
-          }
-          else {
-            array.push({
-              name: list[index],
-              id: i+index+1
-            })
-          }
+        if (list[index] instanceof Array) {
+          array.push({
+            name: list[index][0],
+            id: ++this.pushRank,
+            children: this.getAllFields(list[index], i + index),
+          });
+        } else {
+          array.push({
+            name: list[index],
+            id: ++this.pushRank,
+          });
         }
+      }
       return array;
     },
-    getScholarRank(){
-      var body = {
-        search_word: null,
-        select_items: [],
+    handleSubString(string) {
+      if(string.length>60) {
+        return string.substring(0,60)+"…";
+      }
+      else return string
+    },
+    getScholarRank() {
+      var select_items = "";
+      for (var i = 0; i < this.fieldSelection.length; i++) {
+        if (i != 0) select_items += ",";
+        select_items += this.fieldSelection[i].name;
+      }
+      if (this.fieldSelection.length === 128) {
+        this.select_items = "";
       }
       this.$axios({
-      method: "get",
-      url: "/get_scholars_by_h_index",
-      data: this.$qs.stringify(this.params),
-    })
-      .then((res) => {
-        // res 是 response 的缩写
-        console.log("Scholar:getFields", res.data);
+        method: "post",
+        url: "/get_scholars_by_h_index",
+        data: qs.stringify({
+          select_items: select_items,
+        }),
       })
-      .catch((err) => {
-        //请求若出现路由找不到等其它异常，则在终端输出错误信息
-        console.log(err);
-      });
-    }
+        .then((res) => {
+          this.scholarDesserts = res.data;
+          console.log("Scholar:getFields", res.data);
+        })
+        .catch((err) => {
+          //请求若出现路由找不到等其它异常，则在终端输出错误信息
+          console.log(err);
+        });
+    },
   },
   mounted() {
     this.$nextTick(() => {
       this.getScholarFields();
+      this.getScholarRank();
     });
-  }
+  },
 };
 </script>
 
@@ -535,11 +405,12 @@ export default {
   left: 0;
 }
 .tree-filter {
+  padding-top: 20px;
   border-radius: 20px;
   background-color: #ffffff;
   box-sizing: border-box;
+  border: 2px solid rgba(0, 0, 0, 0.65);
   font-size: vw(16);
-  box-shadow: 2px 2px 10px 0px rgba(0, 0, 0, 0.35);
   width: vw(430);
   height: vh(700);
   overflow: auto;
