d72b1a468aad953b97962bbc650ee64c84c948d2
ann <11@qq.com>
2022-12-13 18:44:58 +0800

    18:44

--- src/views/Home_son/HomeSon3.vue
+++ src/views/Home_son/HomeSon3.vue
@@ -2,97 +2,174 @@
   <div>
     <div style="display: flex">
       <div class="focus_1">
-        <v-btn depressed large @click="into_another_son(1)">关注</v-btn>
+        <v-btn
+          depressed
+          large
+          v-if="user_name"
+          @click="into_another_son(1)"
+          >关注</v-btn
+        >
       </div>
-      <div class="focus_2">
+      <div class="focus_2" v-if="user_name">
         <v-btn depressed large @click="into_another_son(2)">推荐</v-btn>
       </div>
-      <div class="focus_3">
+      <div class="focus_2_2" v-else>
         <v-btn
           depressed
           large
           color="blue-grey lighten-4"
-          @click="into_another_son(3)"
-          >收藏</v-btn
+          @click="into_another_son(2)"
+          >推荐</v-btn
         >
+      </div>
+      <div class="focus_3" v-if="user_name">
+        <v-btn depressed large @click="into_another_son(3)" color="blue-grey lighten-4">收藏</v-btn>
       </div>
     </div>
     <div>
       <v-card
-        class="home_collect_card"
-        v-for="item in collect_content"
+        class="home_focus_card_2"
+        v-for="item in recommand_content"
         :key="item.id"
       >
-        <v-list-item-title class="headline_2_2">{{
-          item.article_name
+        <v-list-item-title class="headline_2">{{
+          item.title
         }}</v-list-item-title>
-        <v-list-item-subtitle class="subtitle_recommand_1_2">{{
-          item.author
-        }}</v-list-item-subtitle>
-        <div class="recommand_book_2">{{ item.book }}</div>
-        <div class="quote_recommand_2">
-          <div>
-            <p class="font-weight-black">
-              Number of citation：{{ item.quote_num }}
-            </p>
-          </div>
-          <div style="margin-left: 20px">
-            <p class="font-weight-black">
-              Number of visits：{{ item.page_view }}
-            </p>
+        <div class="author_rcm">
+          <div
+            v-for="item2 in item.author_name"
+            :key="item2.id"
+            style="float: left"
+          >
+            <v-list-item-subtitle class="subtitle_recommand_1">{{
+              item2
+            }}</v-list-item-subtitle>
           </div>
         </div>
-        <div class="recommand_icon_fa_2">
-          <img src="@/assets/quote.png" class="recommand_icon_1_2" />
-          <img src="@/assets/art_sc_cancel2.png" class="recommand_icon_2_2" />
+        <v-list-item-subtitle class="subtitle_recommand_1">{{
+          item.year
+        }}</v-list-item-subtitle>
+        <div class="recommand_book">Abstract：</div>
+        <div class="recommand_book_2" v-if="item.abstract != 'null'">
+          {{ item.abstract }}
+        </div>
+        <div class="recommand_book_3" v-if="item.abstract == 'null'">-</div>
+        <div style="display: flex">
+          <div class="quote_recommand_fa">
+            <p class="quote_recommand_0">引用量：</p>
+            <p class="quote_recommand">{{ item.n_citation }}</p>
+          </div>
+          <div class="recommand_icon_1" @click="into_detail(item.url[0])">
+            <v-icon color="#232f3d" class="recommand_icon_3" medium> mdi-earth </v-icon>
+            <p class="quote_recommand_1">原文链接</p>
+          </div>
+          <v-icon color="#232f3d" medium class="recommand_icon_2">
+            mdi-star-minus
+          </v-icon>
         </div>
       </v-card>
+      <div class="page_index_2">
+        <v-container>
+          <v-row justify="center">
+            <v-col cols="6">
+              <v-container>
+                <v-pagination
+                  v-model="page"
+                  :length="page_all"
+                  @input="home_get_user_list_2()"
+                ></v-pagination>
+              </v-container>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
+      <v-snackbar
+        v-model="snackbar"
+        :top="y === 'top'"
+        :vertical="mode === 'vertical'"
+      >
+        {{ text }}
+        <template v-slot:action="{ attrs }">
+          <v-btn text v-bind="attrs" @click="snackbar = false"> Close </v-btn>
+        </template>
+      </v-snackbar>
     </div>
   </div>
 </template>
-    
-  <script>
+  
+<script>
+import qs from "qs";
 export default {
   data() {
     return {
-      collect_content: [
-        {
-          article_name:
-            "Curvature-Adaptive Meta-Learning for Fast Adaptation to Manifold Data",
-          author: "Zhi Gao,Yuwei Wu,Mehrtash T Harandi,Yunde Jia",
-          book: "IEEE Transactions on Pattern Analysis and Machine Intelligence (TPAMI) （2022）",
-          quote_num: 0,
-          page_view: 0,
-        },
-        {
-          article_name: "Variational Deep Image Restoration",
-          author: "Jae Woong Soh, Nam Ik Cho",
-          book: "Computer Science、CCF A",
-          quote_num: 0,
-          page_view: 0,
-        },
-      ],
+      recommand_content: [],
+      overlay: 0,
+      page: 1,
+      page_all: 0,
+      user_img: "",
+      user_name: "",
+      user_id: "",
+      user_email: "",
+      user_rcm_total: 0,
+      hover: false,
+      snackbar: false,
+      text: "暂无数据",
+      color: "",
+      mode: "",
+      timeout: 3000,
+      x: null,
+      y: "top",
+      keyword: ["AI", "a"],
+      arr1: [],
+      arr2: [],
+      arr_len: 2,
+      input_keyword: "",
+      add_tmp_str: "",
+      del_tmp_str: "",
     };
+  },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      vm.updateUser();
+    });
   },
   mounted() {},
   methods: {
-    getCollection() {
-      this.$axios({
-        method: "get",
-        url: "/paper_collections",
-        data: qs.stringify({
-          username: "anui",
-          page: 1,
-          pageSize: 5
+    updateUser() {
+      this.user_img = window.localStorage.getItem("user_headshot");
+      this.user_name = window.localStorage.getItem("user_name");
+      this.user_id = window.localStorage.getItem("user_id");
+      this.user_email = window.localStorage.getItem("user_email");
+      if (this.user_name != "") {
+        this.$axios({
+          method: "post",
+          url: "/get_keywords_by_username",
+          data: qs.stringify({
+            username: this.user_name,
+          }),
         })
-      })
         .then((res) => {
-          
+          console.log("add", res.data);
+          this.keyword = res.data.keywords;
+          this.arr_len = res.data.number;
+          for (let i = 0; i < this.arr_len; i++) {
+            this.arr1.push("0");
+            if (i != 0) this.arr2.push("1");
+          }
+          console.log("this.keyword", this.keyword);
+          this.home_get_user_list_2();
+          window.localStorage.setItem("user_keyword", res.data.keywords);
         })
         .catch((err) => {
-          //请求若出现路由找不到等其它异常，则在终端输出错误信息
-          console.log(err);
+          console.log(err.errno);
         });
+      } else {
+        this.keyword = [];
+        this.arr_len = 0;
+        this.arr1 = [];
+        this.arr2 = [];
+        this.home_get_user_list_2();
+      }
     },
     into_another_son(choose_num) {
       console.log(choose_num);
@@ -110,10 +187,133 @@ export default {
         });
       }
     },
+    home_get_user_list_2() {
+      if (this.user_name != "") {
+        this.$axios({
+          method: "post",
+          url: "search",
+          data: {
+            search_word: this.keyword,
+            search_type: this.arr1,
+            search_logic: this.arr2,
+            page: this.page,
+            size: 5,
+            order_type: 2,
+            order: 0,
+          },
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res) => {
+            console.log("rcm_content", res.data);
+            this.page_all = res.data.n_page > 100 ? 100 : res.data.n_page;
+            this.recommand_content = res.data.papers;
+          })
+          .catch((err) => {
+            console.log("err1", err.errno);
+          });
+      }
+      else {
+        this.$axios({
+          method: "post",
+          url: "search",
+          data: {
+            search_word: this.keyword,
+            search_type: this.arr1,
+            search_logic: this.arr2,
+            page: this.page,
+            size: 5,
+            order_type: 2,
+            order: 0,
+          },
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then((res) => {
+          console.log("rcm_content2", res.data);
+          this.page_all = 100;
+          this.recommand_content = res.data.papers;
+        })
+        .catch((err) => {
+          console.log("err2", err.errno);
+        });
+      }
+    },
+    add_subscribe_keyword() {
+      this.keyword.push(this.input_keyword);
+      console.log("this.input_keyword", this.input_keyword);
+      console.log("this.keyword", this.keyword);
+      this.add_tmp_str = "";
+      for (let i = 0; i < this.keyword.length; i++) {
+        if (i != this.keyword.length - 1)
+          this.add_tmp_str = this.add_tmp_str + this.keyword[i] + ",";
+        else this.add_tmp_str = this.add_tmp_str + this.keyword[i];
+      }
+      console.log("this.add_tmp_str", this.add_tmp_str);
+      this.arr1.push("0");
+      this.arr2.push("1");
+      this.$axios({
+        method: "post",
+        url: "/set_keywords_by_username",
+        data: qs.stringify({
+          username: this.user_name,
+          new_keywords: this.add_tmp_str,
+        }),
+      })
+        .then((res) => {
+          this.home_get_user_list_2();
+          console.log("add_keyword", res.data);
+          this.input_keyword = "";
+          this.setData({ input_keyword: "" });
+        })
+        .catch((err) => {
+          console.log("err3", err.errno);
+        });
+    },
+    home_del_subscribe(tmp_item) {
+      this.keyword.splice(this.keyword.indexOf(tmp_item), 1);
+      console.log("tmp_item", tmp_item);
+      console.log("this.keyword", this.keyword);
+      this.arr1.splice(0, 1);
+      this.arr2.splice(0, 1);
+      this.del_tmp_str = "";
+      for (let i = 0; i < this.keyword.length; i++) {
+        if (i != this.keyword.length - 1)
+          this.del_tmp_str = this.del_tmp_str + this.keyword[i] + ",";
+        else this.del_tmp_str = this.del_tmp_str + this.keyword[i];
+      }
+      console.log("this.del_tmp_str", this.del_tmp_str);
+      this.$axios({
+        method: "post",
+        url: "/set_keywords_by_username",
+        data: qs.stringify({
+          username: this.user_name,
+          new_keywords: this.del_tmp_str,
+        }),
+      })
+        .then((res) => {
+          this.home_get_user_list_2();
+          console.log("del_keyword", res.data);
+        })
+        .catch((err) => {
+          console.log("err4", rr.errno);
+        });
+    },
+    into_detail(url_tmp) {
+      console.log(url_tmp);
+      if (url_tmp == null) {
+        this.snackbar = true;
+        this.setData({ snackbar: true });
+      } else {
+        document.location.href = url_tmp;
+      }
+    },
   },
 };
 </script>
-  
+
 <style scoped lang="scss">
 .focus_1 {
   margin-left: vw(110);
@@ -125,50 +325,223 @@ export default {
   color: #232f3d;
   margin-top: vh(20);
 }
+.focus_2_2 {
+  color: #232f3d;
+  margin-top: vh(20);
+}
 .focus_3 {
   margin-left: vw(20);
   color: #232f3d;
   margin-top: vh(20);
 }
-
-.home_collect_card {
+.home_focus_card_2 {
   margin-left: vw(110);
   margin-top: vh(40);
   width: vw(1190);
   padding-top: 10px;
+  padding-bottom: 0.5px;
+  padding-right: vw(20);
 }
-.headline_2_2 {
+.headline_2 {
   margin-left: vw(20);
   font-size: vw(25);
   font-family: "Source Han Sans CN Normal", sans-serif;
 }
-.subtitle_recommand_1_2 {
+.subtitle_recommand_1 {
   margin-left: vw(20);
   font-family: "Source Han Sans CN Normal", sans-serif;
+}
+.author_rcm {
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  overflow: hidden; 
+  text-overflow: ellipsis;
+  -webkit-box-orient: vertical;
+}
+.subtitle_recommand_2 {
+  margin-left: vw(20);
+  font-family: "Source Han Sans CN Normal", sans-serif;
+  color: #90A4AE;
+}
+.recommand_book {
+  color: #455A64;
+  margin-left: vw(20);
+  margin-top: vh(15);
+  margin-right: vw(20);
+  font-weight: bold;
+  font-family: "optima", sans-serif;
 }
 .recommand_book_2 {
-  color: #455a64;
+  color: #455A64;
   margin-left: vw(20);
-  margin-top: vh(10);
-  font-family: "Source Han Sans CN Normal", sans-serif;
+  margin-top: vh(5);
+  margin-right: vw(20);
+  font-family: "optima", sans-serif;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  overflow: hidden; 
+  text-overflow: ellipsis;
+  -webkit-box-orient: vertical;
 }
-.quote_recommand_2 {
+.recommand_book_3 {
+  color: #455A64;
+  margin-left: vw(20);
+  margin-top: vh(5);
+  margin-right: vw(20);
+  font-family: "SourceHanSerifCN", sans-serif;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  overflow: hidden; 
+  text-overflow: ellipsis;
+  -webkit-box-orient: vertical;
+}
+.quote_recommand_fa {
+  width: vw(1010);
+  margin-bottom: vh(10);
+  margin-top: vh(20);
+}
+.quote_recommand {
+  margin-left: vw(5);
+  margin-top: vh(17);
+  display: flex;
+  font-family: "optima", sans-serif;
+  font-weight: bolder;
+  font-size: vw(19);
+}
+.quote_recommand_0 {
   margin-left: vw(20);
   margin-top: vh(15);
   display: flex;
+  font-family: "SourceHanSerifCN", sans-serif;
+  font-weight: bolder;
+  font-size: vw(19);
+  float: left;
+}
+.quote_recommand_1 {
+  margin-left: vw(5);
+  display: flex;
+  font-family: "SourceHanSerifCN", sans-serif;
+  font-size: vw(17);
+  margin-top: vh(48);
+}
+.recommand_icon_fa {
+  display: flex;
+  margin-left: vw(1130);
+}
+.recommand_icon_2 {
+  width: vw(36);
+  margin-left: vw(40);
+  margin-bottom: vh(30);
+}
+.recommand_icon_3 {
+  margin-top: vh(17);
+}
+.add_keyword_class {
+  color: white;
+  margin-left: vw(30);
+  margin-top: vh(25);
+}
+.add_keyword_class_2 {
+  color: white;
+  margin-left: vw(20);
+  margin-top: vh(25);
+}
+.display_box_1 {
+  background-color: rgba(255,255,255,0.97);
+  margin-bottom: vh(30);
+  width: vw(1250);
+  border-radius: vw(10);
+  padding: vw(20);
+}
+.display_botton_1 {
+  text-align: center;
+}
+.headline_display_1 {
+  font-weight: 550;
+  font-size: 1rem;
+  letter-spacing: 0.009375rem;
+  color: #232f3d;
+  margin-top: vh(10);
   font-family: "Source Han Sans CN Normal", sans-serif;
 }
-.recommand_icon_fa_2 {
+.headline_display_2 {
+  font-weight: 500;
+  font-size: 0.9rem;
+  letter-spacing: 0.008375rem;
+  color: #90A4AE;
+  margin-left: vw(10);
+  margin-top: vh(10);
+  font-family: "Source Han Sans CN Normal", sans-serif;
+}
+.headline_display_3 {
+  font-weight: 550;
+  font-size: 1rem;
+  letter-spacing: 0.009375rem;
+  color: #232f3d;
+  margin-top: vh(70);
+  font-family: "Source Han Sans CN Normal", sans-serif;
+}
+.headline_display_4 {
+  font-weight: 500;
+  font-size: 0.9rem;
+  letter-spacing: 0.008375rem;
+  color: #90A4AE;
+  margin-left: vw(10);
+  margin-top: vh(70);
+  font-family: "Source Han Sans CN Normal", sans-serif;
+}
+.display_item_1 {
+  color: #232f3d;
+  display: flex;
+  margin-top: vh(20);
+}
+.display_item_2 {
+  background-color: #232f3d;
+  margin-right: vw(20);
+  padding: vw(10);
+  border-radius: vw(10);
+  font-family: "optima", sans-serif;
+  color: whitesmoke;
+}
+.display_item_3 {
+  color: #232f3d;
   display: flex;
 }
-.recommand_icon_1_2 {
-  margin-left: vw(1070);
-  width: vw(30);
-  margin-bottom: vh(30);
+.display_item_4 {
+  background-color: #232f3d;
+  margin-right: vw(20);
+  padding: vw(10);
+  border-radius: vw(10);
+  box-shadow: 0 0 5px 1px #232f3d;
+  margin-bottom: vh(20);
+  font-family: "Source Han Sans CN Normal", sans-serif;
 }
-.recommand_icon_2_2 {
-  width: vw(35);
-  margin-left: vw(25);
-  margin-bottom: vh(30);
+.display_item_4:hover {
+  background-color: #37474F;
+  margin-right: vw(20);
+  color: white;
+  padding: vw(10);
+  border-radius: vw(10);
+  box-shadow: 0 0 5px 1px #37474F;
+  margin-bottom: vh(20);
+  font-family: "Source Han Sans CN Normal", sans-serif;
+}
+.add_key_focus_1 {
+  margin-top: vh(30);
+}
+.home_icon_5 {
+  width: vw(25);
+  height: vw(25);
+}
+.page_index_2 {
+  text-align: center;
+  margin-top: vh(10);
+}
+.recommand_icon_1 {
+  display: flex;
+}
+.recommand_icon_2 {
+  margin-top: vh(49);
+  margin-left: vw(10);
 }
 </style>
